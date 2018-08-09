3c87259904b7627064c75a93af8b927bb1aa99a9
Admin <jmssri31@gmail.com>
2018-08-09 11:40:15 +0530

    initial

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,23 @@
+import {Component,Input, OnInit} from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+private text='';
+constructor(){}
+ngOnInit(){}
+  
+@Input()
+  OnClickMe(text)
+  {
+    console.log(text);
+  }
+}
+
+  
+
